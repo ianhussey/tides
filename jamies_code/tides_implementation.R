ca6d02022c4dca6341edd634e7e51d13383fb1cc
Jamie Cummins <jamie.cummins@ugent.be>
2024-06-14 20:15:01 +0200

    add hacky quadratic function

--- jamies_code/tides_implementation.R
+++ jamies_code/tides_implementation.R
@@ -14,5 +14,50 @@ tides_test <- function(sample_size, scale_lower_bound,
 
 tides_test(sample_size = 350, 
            scale_lower_bound = 1, 
-           scale_upper_bound = 7, 
-           score = 0)
+           scale_upper_bound = 6, 
+           score = 2.5)
+
+# quadratic approximation 
+fit_quadratic <- function(scale_lower_bound, scale_upper_bound) {
+  
+  data <- tibble(
+    x = c(scale_lower_bound, (scale_lower_bound + scale_upper_bound) / 2, scale_upper_bound),
+    y = c(0, (scale_upper_bound - scale_lower_bound) / 2, 0)
+  )
+  
+  # Fit a quadratic model
+  model <- lm(y ~ poly(x, 2, raw = TRUE), data = data)
+  
+  return(model)
+}
+
+
+
+# check if the y value exceeds the quadratic curve
+check_y_value <- function(model, x, y) {
+  
+  # Predict sd using the quadratic model
+  y_pred <- predict(model, newdata = tibble(x = x))
+  
+  # Check if the given y value exceeds the predicted y value
+  exceeds <- y > y_pred
+  
+  return(exceeds)
+}
+
+
+# Fit model
+model <- fit_quadratic(scale_lower_bound = 1,
+                       scale_upper_bound = 6)
+
+# Coordinates to check
+x <- 2.5
+y <- 2.5
+
+preds <- predict(model, tibble(x = seq(1, 6, .005)))
+
+plot(preds)
+
+# Check if the y value exceeds the quadratic curve
+exceeds <- check_y_value(model, x, y)
+print(exceeds)
