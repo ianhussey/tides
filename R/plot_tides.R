e1e5a8dcb6a688f9a0c6ee60d5bdad5948b58edb
Ian Hussey <ianhusseysipod@gmail.com>
2024-06-25 13:09:52 +0100

    Update plot_tides.R

--- R/plot_tides.R
+++ R/plot_tides.R
@@ -8,7 +8,7 @@
 #' @param min numeric variable representing the variable's minimum possible/observable score.
 #' @param max numeric variable representing the variable's maximum possible/observable score.
 #' @param calculate_min_sd logical variable representing whether a minimum SD should also be calculated. This should only be calculated if the varible is not only trucated (has a minimum and maximum possible/observable score) but also the variable is discrete/binned/granular: ie the response must be whole numbers (e.g., a 1-7 likert scale, where an indiviudal cannot provide a score of 1.5).
-plot_tides <- function(mean, sd, n, min, max, calculate_min_sd){
+plot_tides <- function(mean, sd, n, min, max, calculate_min_sd = FALSE){
   dat <- tibble(mean = mean, 
                 sd = sd,
                 n = n,
@@ -75,3 +75,4 @@ plot_tides <- function(mean, sd, n, min, max, calculate_min_sd){
   
   return(p)
 }
+
