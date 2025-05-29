ddeccdedfa1918a547f2688780a7b8ac48935d30
Ian Hussey <ianhusseysipod@gmail.com>
2025-05-29 20:54:50 +0200

    fixed documentation file

--- R/zzz.R
+++ R/zzz.R
@@ -1,10 +1,4 @@
 #' TIDES
 #'
-#' Check reported Means, SDs and Ns from truncated data (data where the measure has a min and max score) for inconsistencies.
-#'
-#' @section Important:
-#' Package is in early stage development.
-#'
-#' @docType package
-#' @name TIDES
-NULL
+"_PACKAGE"
+#> [1] "_PACKAGE"
