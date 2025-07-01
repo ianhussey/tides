6bdbe93c7e80e56a241b5096f19220b994e8e29b
Ian Hussey <ianhusseysipod@gmail.com>
2025-07-01 17:11:49 +0200

    updated precision()
    - digits rather than precision, to match other functions

--- R/umbrella.R
+++ R/umbrella.R
@@ -12,7 +12,7 @@
 #' @param max       Numeric. Maximum possible (or observed) score.
 #' @param n_items   Integer(1). Number of discrete items per participant
 #'                  (e.g. Likert‐scale items). Defaults to 1.
-#' @param precision Integer. Number of decimal places in reported means/SDs;
+#' @param digits    Integer. Number of decimal places in reported means/SDs;
 #'                  used to define the mean‐grid and to restore trailing zeros.
 #'                  Defaults to 2.
 #'
@@ -26,7 +26,7 @@
 #' @examples
 #' \dontrun{
 #' # All possible means and SDs for N=14, scale 1–7, two decimal places:
-#' df <- umbrella(n = 14, min = 1, max = 7, n_items = 1, precision = 2)
+#' df <- umbrella(n = 14, min = 1, max = 7, n_items = 1, digits = 2)
 #' head(df)
 #' }
 #'
@@ -37,34 +37,34 @@
 #' @importFrom scrutiny restore_zeros grimmer
 #' @importFrom dplyr filter mutate select
 #' @export
-umbrella <- function(n, min, max, n_items = 1, precision = 2){
+umbrella <- function(n, min, max, n_items = 1, digits = 2){
   
   res <- 
     # 1. find all GRIM consistent means and the min/max of their SD
     
-    # define all possible values of mean from min to max in increments of precision
-    tibble(mean = seq(from = min, to = max, by = 10^-precision)) |> 
+    # define all possible values of mean from min to max in increments of digits
+    tibble(mean = seq(from = min, to = max, by = 10^-digits)) |> 
     # generate min and max SD for each mean, which may ba NA. This generates only TIDES consistent values within the bounds.
     mutate(sd_bounds = map(mean, ~ sd_bounds(mean    = .x,
                                              n       = n,
                                              min     = min,
                                              max     = max,
                                              n_items = n_items,
-                                             digits  = precision))) |>
+                                             digits  = digits))) |>
     unnest(sd_bounds) |>
     # drop means for which no feasible SD‐range exists, so that your remaining means are now GRIM consistent with at least the min and max also being GRIMMER consistent 
     filter(!is.na(min_sd), !is.na(max_sd)) |>
     
     # 2. find all GRIMMER consistent SDs
     
-    # for each remaining mean, generate all SDs between the min and max bound in increments of precision
+    # for each remaining mean, generate all SDs between the min and max bound in increments of digits
     mutate(sd = map2(min_sd, max_sd, ~ seq(.x, .y, by = 0.01))) |>
     unnest(sd) |>
     select(mean, sd) |>
     # test which of these SDs are GRIMMER consistent
     ## create variables needed for GRIMMER testing
     mutate(n         = n,
-           precision = precision,
+           digits    = digits,
            n_items   = n_items,
            min       = min,
            max       = max,
@@ -72,9 +72,9 @@ umbrella <- function(n, min, max, n_items = 1, precision = 2){
            rounding = "up") |>
     ## convert M and SD to character and then restore trailing zero, as required for GRIM/MER
     mutate(mean_char = as.character(mean),
-           mean_char = scrutiny::restore_zeros(mean_char, width = precision),
+           mean_char = scrutiny::restore_zeros(mean_char, width = digits),
            sd_char = as.character(sd),
-           sd_char = scrutiny::restore_zeros(sd_char, width = precision)) |>
+           sd_char = scrutiny::restore_zeros(sd_char, width = digits)) |>
     ## apply GRIMMER to the reduced grid
     mutate(grimmer = purrr::pmap(list(x        = mean_char,
                                       sd       = sd_char,
