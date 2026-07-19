7d2d12a85333603d4400dc8c9e1b2f25a9ab5a38
Ian Hussey <ianhusseysipod@gmail.com>
2026-07-19 12:59:48 +0200

    standardized build.R

--- build.R
+++ build.R
@@ -5,25 +5,24 @@ setwd("~/git/tides")
 
 devtools::document()
 
-devtools::check(vignettes = FALSE)
+devtools::check()
 
-#devtools::install(build_vignettes = TRUE)
-#vignette("tides")
+# once you have the package updated, you can use it to build the vignettes, check the whole thing, and reinstall again
+devtools::build_vignettes()
 
-# or from github, after push
-devtools::install_github("ianhussey/tides")
+devtools::install(build_vignettes = TRUE)
+
+# # or from github, after push
+# devtools::install_github("ianhussey/tides")
 
 library(tides)
 
 ?tides
-
 vignette("tides")
+
 
 detach("package:tides", unload = TRUE)
 
-# once you have the package updated, you can use it to build the vignettes, check the whole thing, and reinstall again
-devtools::build_vignettes()
-devtools::check()
 
 # cran checks
 # win-builder
@@ -32,5 +31,4 @@ check_win_devel() # emails results to the maintainer address in DESCRIPTION
 
 # R-hub
 library(rhub)
-# rhub_setup() # one time
 rhub_check() # v2: runs on GitHub Actions in your repo
